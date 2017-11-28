0d6fd9fc3d5ce6400d2b14461c0d7a7cf1be8947
Cristian Cortez <cortez.cristian@gmail.com>
2017-11-28 14:23:38 -0300

    Demo csv read and parse

--- demo-promises/js/app.js
+++ demo-promises/js/app.js
@@ -2,12 +2,34 @@ angular
   .module('futbol', [])
   .controller('futbolCtrl', function($scope, leerTXT) {
     $scope.mensaje = '';
-    leerTXT.leerJugador().then(function(d){
+    leerTXT.leerGoles('17').then(function(d){
       $scope.mensaje = d;
+    }).catch(function(){
+      // Fallo cargar jugadores
     });
   })
   .factory('leerTXT', function($http, $q) {
     return {
+      leerGoles: function() {
+        var p = $q.defer();
+
+        $http({
+          method: 'GET',
+          url: './goles.csv'
+        }).then(function(resp){
+          var jugadores = [];
+          var lines = resp.data.split('\n');
+          angular.forEach(lines, function(line){
+            var l = line.split(',');
+            jugadores.push({nombre: l[0], goles: l[1] })
+          });
+          p.resolve(jugadores);
+        }).catch(function(err){
+          p.reject(err);
+        });
+
+        return p.promise;
+      },
       leerJugador: function() {
         var p = $q.defer();
 
@@ -16,7 +38,9 @@ angular
           url: './data.txt'
         }).then(function(resp){
           p.resolve(resp.data);
-        })
+        }).catch(function(err){
+          p.reject(err);
+        });
 
         return p.promise;
       },
