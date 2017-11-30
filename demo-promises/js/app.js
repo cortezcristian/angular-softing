2a2cb577e710b7013eb0ddde25aafbab6f18e099
Cristian Cortez <cortez.cristian@gmail.com>
2017-11-30 02:37:30 -0300

    Valor buscado

--- demo-promises/js/app.js
+++ demo-promises/js/app.js
@@ -2,7 +2,7 @@ angular
   .module('futbol', [])
   .controller('futbolCtrl', function($scope, leerTXT) {
     $scope.mensaje = '';
-    leerTXT.leerGoles('17').then(function(d){
+    leerTXT.leerGoles('mess').then(function(d){
       $scope.mensaje = d;
     }).catch(function(){
       // Fallo cargar jugadores
@@ -10,7 +10,7 @@ angular
   })
   .factory('leerTXT', function($http, $q) {
     return {
-      leerGoles: function() {
+      leerGoles: function(valorBuscado) {
         var p = $q.defer();
 
         $http({
@@ -19,9 +19,15 @@ angular
         }).then(function(resp){
           var jugadores = [];
           var lines = resp.data.split('\n');
+          var regexBuscar = new RegExp('.*'+valorBuscado+'.*', 'i');
           angular.forEach(lines, function(line){
             var l = line.split(',');
-            jugadores.push({nombre: l[0], goles: l[1] })
+            if (
+              angular.isString(l[0]) && angular.isString(l[1])
+              && ( l[0].match(regexBuscar) || l[1].match(regexBuscar) )
+              ) {
+              jugadores.push({nombre: l[0], goles: l[1] })
+            }
           });
           p.resolve(jugadores);
         }).catch(function(err){
